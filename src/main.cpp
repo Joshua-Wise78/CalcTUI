162af5e1ce1529a99f69f5a5e0b8458c67935dfc
Joshua Wise <Joshua.Wise722@gmail.com>
2026-07-05 21:03:02 -0400

    operator buttons and eval func

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,65 @@
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
+#include <sstream>
 #include <string>
+
+std::string evaluate(const std::string &expression) {
+  std::stringstream ss(expression);
+  double left_operand, right_operand;
+  char math_operator;
+
+  if (ss >> left_operand >> math_operator >> right_operand) {
+    switch (math_operator) {
+    case '+':
+      return std::to_string(left_operand + right_operand);
+    case '-':
+      return std::to_string(left_operand - right_operand);
+    case '*':
+      return std::to_string(left_operand * right_operand);
+    case '/':
+      if (right_operand == 0)
+        return "Error: Div by 0";
+      return std::to_string(left_operand / right_operand);
+    default:
+      return expression;
+    }
+  }
+  return expression;
+}
 
 int main() {
   using namespace ftxui;
 
   std::string value = "";
 
-  auto on_click = [&](std::string digit) { value = digit; };
+  auto on_click = [&](std::string digit) { value += digit; };
 
   auto btn1 = Button("1", [&] { on_click("1"); });
   auto btn2 = Button("2", [&] { on_click("2"); });
   auto btn3 = Button("3", [&] { on_click("3"); });
-
   auto btn4 = Button("4", [&] { on_click("4"); });
   auto btn5 = Button("5", [&] { on_click("5"); });
   auto btn6 = Button("6", [&] { on_click("6"); });
-
   auto btn7 = Button("7", [&] { on_click("7"); });
   auto btn8 = Button("8", [&] { on_click("8"); });
   auto btn9 = Button("9", [&] { on_click("9"); });
 
+  auto plus = Button("+", [&] { on_click("+"); });
+  auto minus = Button("-", [&] { on_click("-"); });
+  auto divide = Button("/", [&] { on_click("/"); });
+  auto multiply = Button("*", [&] { on_click("*"); });
+
+  auto equals = Button("=", [&] { value = evaluate(value); });
+  auto clear = Button("C", [&] { value = ""; });
+
   auto row1 = Container::Horizontal({btn1, btn2, btn3});
   auto row2 = Container::Horizontal({btn4, btn5, btn6});
   auto row3 = Container::Horizontal({btn7, btn8, btn9});
-  auto keypad = Container::Vertical({row1, row2, row3});
+  auto row4 = Container::Horizontal({plus, minus, divide, multiply});
+  auto row5 = Container::Horizontal({clear, equals});
+
+  auto keypad = Container::Vertical({row1, row2, row3, row4, row5});
 
   auto calculator_ui = Renderer(keypad, [&] {
     return vbox({
@@ -43,6 +77,16 @@ int main() {
             vbox({btn7->Render() | flex}) | border,
             vbox({btn8->Render() | flex}) | border,
             vbox({btn9->Render() | flex}) | border,
+        }),
+        hbox({
+            vbox({plus->Render() | flex}) | border,
+            vbox({minus->Render() | flex}) | border,
+            vbox({divide->Render() | flex}) | border,
+            vbox({multiply->Render() | flex}) | border,
+        }),
+        hbox({
+            vbox({clear->Render() | flex}) | border,
+            vbox({equals->Render() | flex}) | border,
         }),
         text(" Output: " + value) | border,
     });
